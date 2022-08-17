1f1d8ecb2764ec01750754e7074a835a2393fe4e
denis <denis-110889@yandex.ru>
2022-08-17 10:34:35 +0300

    17082022 14 работа122

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
